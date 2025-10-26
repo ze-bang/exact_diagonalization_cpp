6267ba34bd8d9f61d014ad1a31e90d2ccfab93a5
ze-bang <zhouzb79@gmail.com>
2025-10-26 04:47:03 -0400

    clean up

--- src/gpu_utils.cu
+++
@@ -1,9 +0,0 @@
-// gpu_utils.cu - GPU utility function implementations
-#include "gpu_utils.cuh"
-
-namespace gpu {
-
-// All device functions are defined inline in gpu_utils.cuh
-// This file is kept for potential future non-inline implementations
-
-} // namespace gpu
